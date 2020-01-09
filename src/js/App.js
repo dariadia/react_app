7c58938950a0170c6a481eeaa3bb07722ec8d8c0
Daria Diachkova <dariadiachkova@mail.ru>
2020-01-09 09:56:04 +0400

    set up as classes

--- src/js/App.js
+++ src/js/App.js
@@ -3,19 +3,23 @@ import logo from "../assets/logo.svg";
 import "../styles/App.css";
 import Image from "./Image.js";
 
-const App = () => {
-  const Images = [
-    { id: 1, src: "./assets/cat.jpg", title: "Mr Whiskers" },
-    { id: 2, src: "./assets/kitty.jpeg", title: "Snowball" }
-  ].map(img => <Image src={img.src} title={img.title} key={img.id} />);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {Images}
-      </header>
-    </div>
-  );
-};
+class App extends React.Component {
+  getImages() {
+    return [
+      { id: 1, src: "./assets/cat.jpg", title: "Mr Whiskers" },
+      { id: 2, src: "./assets/kitty.jpeg", title: "Snowball" }
+    ].map(img => <Image src={img.src} title={img.title} key={img.id} />);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {this.getImages()}
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
