11b3b446f0308d95eabb5478c923ad28eaf0f0fd
Daria Diachkova <dariadiachkova@mail.ru>
2020-01-09 15:10:07 +0400

    to do up set up

--- src/js/App.js
+++ src/js/App.js
@@ -1,43 +1,38 @@
 import React, { Component } from "react";
-import logo from "../assets/logo.svg";
 import "../styles/App.css";
-import Image from "./Image.js";
+import "../styles/style.css";
+import TodoItem from "./TodoItem";
+import todosData from "./todosData";
 
 class App extends Component {
   constructor() {
     super();
-    this.images = [
-      { id: 1, src: "./assets/cat.jpg", title: "Mr Whiskers" },
-      { id: 2, src: "./assets/kitty.jpeg", title: "Snowball" }
-    ];
-    this.state = { count: 0 };
-    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      todos: todosData
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  getImages() {
-    return this.images.map(img => (
-      <Image src={img.src} title={img.title} key={img.id} says={img.says} />
-    ));
-  }
-  handleClick() {
+  handleChange(id) {
     this.setState(prevState => {
-      return { count: prevState.count + 1 };
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos
+      };
     });
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {this.getImages()}
-        </header>
-        <div>
-          <p>{this.state.count}</p>
-          <button onClick={this.handleClick}>Click Here!</button>
-        </div>
-      </div>
-    );
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return <div className="todo-list">{todoItems}</div>;
   }
 }
 
---
+++ src/js/todosData.js
@@ -0,0 +1,19 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Take out the trash",
+    completed: true
+  },
+  {
+    id: 2,
+    text: "Do the groceries",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Mow the lawn",
+    completed: true
+  }
+];
+
+export default todosData;
