dcbd473fff44ae31e1a580c896ab7f9f3e248c42
Daria Diachkova <dariadiachkova@mail.ru>
2020-01-10 21:30:02 +0400

    added search

--- src/js/App.js
+++ src/js/App.js
@@ -7,8 +7,10 @@ class App extends Component {
     super();
     this.state = {
       characters: [],
-      loading: true
+      loading: true,
+      search: ""
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -19,15 +21,43 @@ class App extends Component {
   }
 
   renderCharacters() {
-    return this.state.characters.map(character => (
-      <CharacterCard aboutCharacter={character} key={character.name} />
-    ));
+    return this.state.characters.map(
+      character =>
+        Object.values(character).map(value =>
+          value
+            //есть ли введенный символ среди всех значений объекта
+            .includes(this.state.search)
+            //получаем массив совпадений типа [[true,false],[false,true]], сокращаем до [1,1]
+            .map(el => el.reduce((a, b) => a + b))
+            //сокращаем, если >= 1, то хотя бы одно значение было true, нам подходит этот персонаж
+            .reduce((a, b) => a + b)
+        ) >= 1 && (
+          <CharacterCard aboutCharacter={character} key={character.name} />
+        )
+    );
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   }
 
   render() {
     const content = this.state.loading ? "loading..." : this.renderCharacters();
-    setTimeout(console.log("Hi!"), 1000); // после каждого render, сейчас вызывается 3 раза
-    return <div className="container">{content}</div>;
+    setTimeout(console.log("Rendered!"), 1000); // после каждого render
+    return (
+      <div className="container">
+        <input
+          type="text"
+          value={this.state.search}
+          name="search"
+          placeholder="Search for ..."
+          onChange={this.handleChange}
+        />
+        <br />
+        {content}
+      </div>
+    );
   }
 }
 
